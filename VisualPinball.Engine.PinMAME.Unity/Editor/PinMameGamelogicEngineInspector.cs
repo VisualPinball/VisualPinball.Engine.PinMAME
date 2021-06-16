075493ad377bedd35f846abaa8c1c26a17cc6b89
jsm174 <jsm174@gmail.com>
2021-06-16 16:53:56 -0400

    games: Add CFTBL (Creature From The Black Lagoon)

--- VisualPinball.Engine.PinMAME.Unity/Editor/PinMameGamelogicEngineInspector.cs
+++ VisualPinball.Engine.PinMAME.Unity/Editor/PinMameGamelogicEngineInspector.cs
@@ -50,6 +50,7 @@ namespace VisualPinball.Engine.PinMAME.Editor
 			_gle = (PinMameGamelogicEngine) target;
 			_games = new PinMameGame[] {
 				new AttackFromMars(),
+				new CreatureFromTheBlackLagoon(),
 				new MedievalMadness(),
 				new Terminator2(),
 				new FlashGordon(),
