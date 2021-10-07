f4b368ae09997015c142c5b5fe02897cdc741a49
freezy <freezy@kodi.tv>
2021-10-07 22:56:14 +0200

    lights: Add fading steps.

--- VisualPinball.Engine.PinMAME/Games/Terminator2.cs
+++ VisualPinball.Engine.PinMAME/Games/Terminator2.cs
@@ -174,11 +174,11 @@ namespace VisualPinball.Engine.PinMAME.Games
 			new GamelogicEngineLamp("87") { Description = "Top Lane Center" },
 			new GamelogicEngineLamp("88") { Description = "Top Lane Right" },
 
-			new GamelogicEngineLamp("0") { Description = "GI: Top Insert", Source = LampSource.GI },
-			new GamelogicEngineLamp("1") { Description = "GI: Bottom Insert", Source = LampSource.GI },
-			new GamelogicEngineLamp("2") { Description = "GI: Right Playfield", Source = LampSource.GI },
-			new GamelogicEngineLamp("3") { Description = "GI: CPU String", Source = LampSource.GI },
-			new GamelogicEngineLamp("4") { Description = "GI: Left Playfield", Source = LampSource.GI },
+			new GamelogicEngineLamp("0") { Description = "GI: Top Insert", Source = LampSource.GI, FadingSteps = 8 },
+			new GamelogicEngineLamp("1") { Description = "GI: Bottom Insert", Source = LampSource.GI, FadingSteps = 8 },
+			new GamelogicEngineLamp("2") { Description = "GI: Right Playfield", Source = LampSource.GI, FadingSteps = 8 },
+			new GamelogicEngineLamp("3") { Description = "GI: CPU String", Source = LampSource.GI, FadingSteps = 8 },
+			new GamelogicEngineLamp("4") { Description = "GI: Left Playfield", Source = LampSource.GI, FadingSteps = 8 },
 		};
 
 		protected override GamelogicEngineCoil[] GameCoils { get; } = {
