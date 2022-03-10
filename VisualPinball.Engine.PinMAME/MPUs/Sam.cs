2d7f4d12159a8172bf24662827a9ec75924dac30
Jason Millard <jsm174@gmail.com>
2022-03-10 08:32:31 -0500

    sam: update cabinet switches

--- VisualPinball.Engine.PinMAME/MPUs/Sam.cs
+++ VisualPinball.Engine.PinMAME/MPUs/Sam.cs
@@ -14,9 +14,36 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+using VisualPinball.Engine.Common;
+using VisualPinball.Engine.Game.Engines;
+
 namespace VisualPinball.Engine.PinMAME.MPUs
 {
-	public abstract class Sam : Wpc
+	public abstract class Sam : PinMameGame
 	{
+		public override GamelogicEngineSwitch[] AvailableSwitches => Concat(_switches, Switches);
+
+		protected override GamelogicEngineCoil[] Coils => Concat(_coils, GameCoils);
+		protected abstract GamelogicEngineCoil[] GameCoils { get; }
+
+		protected abstract GamelogicEngineSwitch[] Switches { get; }
+
+		private readonly GamelogicEngineSwitch[] _switches = {
+			new GamelogicEngineSwitch(SwCoin1, 65) { Description = "Coin Button 1", InputActionHint = InputConstants.ActionInsertCoin1, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwCoin2, 66) { Description = "Coin Button 2", InputActionHint = InputConstants.ActionInsertCoin2, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwCoin3, 67) { Description = "Coin Button 3", InputActionHint = InputConstants.ActionInsertCoin3, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwCancel, -3) { Description = "Cancel", InputActionHint = InputConstants.ActionCoinDoorCancel, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwDown, -2) { Description = "Down", InputActionHint = InputConstants.ActionCoinDoorDown, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwUp, -1) { Description = "Up", InputActionHint = InputConstants.ActionCoinDoorUp, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwEnter, 0) { Description = "Enter", InputActionHint = InputConstants.ActionCoinDoorEnter, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwStartButton, 16) { Description = "Start", InputActionHint = InputConstants.ActionStartGame, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwSlamTilt, -6) { Description = "Slam Tilt", InputActionHint = InputConstants.ActionSlamTilt, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwTilt, -7) { Description = "Tilt" },
+			new GamelogicEngineSwitch(SwFlipperLowerRight, 82) { Description = "Lower Right Flipper", InputActionHint = InputConstants.ActionRightFlipper, InputMapHint = InputConstants.MapCabinetSwitches },
+			new GamelogicEngineSwitch(SwFlipperLowerLeft, 84) { Description = "Lower Left Flipper", InputActionHint = InputConstants.ActionLeftFlipper, InputMapHint = InputConstants.MapCabinetSwitches },
+		};
+
+		private readonly GamelogicEngineCoil[] _coils = {
+		};
 	}
 }
